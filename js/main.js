2a5ee38d39f69aa9aaf75739ea85d39b83ae1f26
Ângela Igreja <angel.igreja@gmail.com>
2015-05-18 00:53:07 +0200

    Update my personal website.

--- js/main.js
+++ js/main.js
@@ -8,4 +8,48 @@
         }
     })
 
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '30px',
+            maxFontSize: '60px'
+        }
+    );
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    $(function() {
+        for (var i = 0; i < 1; i++){
+            $('#proj0').bind('click', function(e) {
+                e.preventDefault();
+                $('#popup0').bPopup({
+                    modalClose: true,
+                    opacity: 0.6,
+                    positionStyle: 'fixed', //'fixed' or 'absolute'
+                    fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
+                    followSpeed: 1500 //can be a string ('slow'/'fast') or int
+                });
+            });
+        }
+    });
+
 })(); // End of use strict
